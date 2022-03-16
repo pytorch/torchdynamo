d4eeb5bac20194dd96ee1063a50e36167260ee01
Animesh Jain <anijain@umich.edu>
2022-03-15 22:06:28 -0700

    Using mean instead of sum to have reasonable loss value for backprop (#51)

--- torchbench.py
+++ torchbench.py
@@ -79,7 +79,7 @@ ONLY_EVAL_DATASET = {"yolov3"}
 
 # These models support only train mode. So accuracy checking can't be done in
 # eval mode.
-ONLY_TRAINING_MODE = {"tts_angular"}
+ONLY_TRAINING_MODE = {"tts_angular", "tacotron2"}
 
 current_name = ""
 current_device = ""
