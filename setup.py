c4373121fb74f0300e20345b79700eb4c86a63be
Jason Ansel <jansel@jansel.net>
2022-03-17 16:46:31 -0700

    Fill in missing fields in setup.py

--- setup.py
+++ setup.py
@@ -7,11 +7,28 @@ from torch.utils.cpp_extension import CppExtension
 
 assert (3, 7) <= sys.version_info < (3, 9), "requires python 3.7 or 3.8"
 
+long_description = """
+TorchDynamo is a Python-level JIT compiler designed to make unmodified
+PyTorch programs faster. TorchDynamo hooks into the frame evaluation API
+in CPython (PEP 523) to dynamically modify Python bytecode right before
+it is executed. It rewrites Python bytecode in order to extract sequences
+of PyTorch operations into an FX Graph which is then just-in-time
+compiled with an ensemble of different backends and autotuning.
+"""
+
 setup(
     name="torchdynamo",
-    version="0.1",
+    version="0.2.0",
+    url="https://github.com/facebookresearch/torchdynamo",
+    description="A Python-level JIT compiler designed to make unmodified PyTorch programs faster.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author="Jason Ansel",
     author_email="jansel@fb.com",
+    license="BSD-3",
+    keywords="pytorch machine learning compilers",
+    python_requires=">=3.8, <3.9",
+    install_requires=["torch>=1.11.0", "tabulate"],
     packages=["torchdynamo"],
     ext_modules=[
         Extension(
