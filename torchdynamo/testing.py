74ebe2f4618da648a92de03a4efc7d6bfb8e1ba9
Wei Wei <wwei6@fb.com>
2022-03-29 16:16:44 -0700

    fix lint

--- torchdynamo/testing.py
+++ torchdynamo/testing.py
@@ -70,7 +70,9 @@ def same(a, b, cos_similarity=False):
     """Check correctness to see if a and b match"""
     if isinstance(a, (list, tuple, torch.nn.ParameterList, torch.Size)):
         assert isinstance(b, (list, tuple)), f"type mismatch {type(a)} {type(b)}"
-        return len(a) == len(b) and all(same(ai, bi, cos_similarity) for ai, bi in zip(a, b))
+        return len(a) == len(b) and all(
+            same(ai, bi, cos_similarity) for ai, bi in zip(a, b)
+        )
     elif isinstance(a, dict):
         assert isinstance(b, dict)
         assert set(a.keys()) == set(
@@ -108,7 +110,10 @@ def same(a, b, cos_similarity=False):
         "Variable",
     ):
         assert type(a) is type(b)
-        return all(same(getattr(a, key), getattr(b, key), cos_similarity) for key in a.__dict__.keys())
+        return all(
+            same(getattr(a, key), getattr(b, key), cos_similarity)
+            for key in a.__dict__.keys()
+        )
     else:
         raise RuntimeError(f"unsupported type: {type(a).__name__}")
 
