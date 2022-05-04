d2ab4756619fd76d521884ea2151f06b7f32ec70
Jason Ansel <jansel@jansel.net>
2022-05-03 18:02:39 -0700

    Fix for `KeyError: 'Size'` error
    Traceback (most recent call last):
      File "/home/jansel/torchdynamo/torchdynamo/convert_frame.py", line 170, in _convert_frame_assert
        code = transform_code_object(frame.f_code, transform)
      File "/home/jansel/torchdynamo/torchdynamo/bytecode_transformation.py", line 340, in transform_code_object
        fix_vars(instructions, code_options)
      File "/home/jansel/torchdynamo/torchdynamo/bytecode_transformation.py", line 307, in fix_vars
        instructions[i].arg = names[instructions[i].argval]
    KeyError: 'Size'

--- torchbench.py
+++ torchbench.py
@@ -1095,6 +1095,8 @@ def run_one_model(
     cos_similarity=False,
     skip_accuracy_check=False,
 ):
+    t0 = time.perf_counter()
+
     tolerance = 1e-4
     # Increase the tolerance for torch allclose
     if is_training and current_device == "cuda" and name in REQUIRE_HIGHER_TOLERANCE:
@@ -1152,7 +1154,9 @@ def run_one_model(
             frames_third_pass = 0
 
         if output_filename and "coverage" in output_filename:
-            results.append(f"{ok:3}/{total:3} +{frames_third_pass} frames")
+            results.append(
+                f"{ok:3}/{total:3} +{frames_third_pass} frames {time.perf_counter()-t0:.0f}s"
+            )
 
         results.append(experiment(model, example_inputs))
         print(" ".join(map(str, results)))
