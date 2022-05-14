4307ec12757fe48f282930120d65ee3c2e3cb13e
Michael Voznesensky <voznesenskym@gmail.com>
2022-05-13 20:02:13 -0700

    lint a file I plan to delete

--- test.py
+++ test.py
@@ -4,6 +4,7 @@ import torchdynamo
 import threading
 import time
 
+
 def toy_example(a, b):
     x = a / (torch.abs(a) + 1)
     if b.sum() < 0:
@@ -15,7 +16,7 @@ def my_compiler(gm: torch.fx.GraphModule, example_inputs: List[torch.Tensor]):
     gm.graph.print_tabular()
     if not hasattr(gm, 'count_voz'):
         setattr(gm, 'count_voz', 1)
-    else:    
+    else:
         setattr(gm, 'count_voz', getattr(gm, 'count_voz') + 1)
 
     print(f" Total calls: {getattr(gm, 'count_voz') + 1}")
@@ -25,6 +26,7 @@ def my_compiler(gm: torch.fx.GraphModule, example_inputs: List[torch.Tensor]):
 def foo():
     with torchdynamo.optimize(my_compiler, nopython=False):
         toy_example(torch.randn(10), torch.randn(10))
+
 
 print(f"Main on {threading.get_ident()}")
 ts = list()
