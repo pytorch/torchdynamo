c30f2364c6ac2767d92da8ef1408ec3b6d7c7630
Animesh Jain <anijain@umich.edu>
2022-06-15 14:03:14 -0700

    Suppress warnings during the pre-checks on a frame (#396)
    * Suppress warnings during the pre-checks on a frame
    
    * Encapsulate in PatchTorch

--- torchdynamo/convert_frame.py
+++ torchdynamo/convert_frame.py
@@ -19,6 +19,7 @@ from .bytecode_analysis import remove_dead_code
 from .bytecode_analysis import remove_pointless_jumps
 from .bytecode_transformation import is_generator
 from .bytecode_transformation import transform_code_object
+from .eval_frame import TorchPatcher
 from .eval_frame import WrapperBackend
 from .eval_frame import always_optimize_code_objects
 from .eval_frame import skip_code
@@ -125,6 +126,7 @@ def wrap_convert_context(fn):
     return _fn
 
 
+@TorchPatcher.suppress_torch_distributed_warnings
 def has_tensor_in_frame(frame):
     """Check if the frame has torch.* related bits"""
     # Check if the function was decorated with torchdynamo.optimize
