af1160965bd64ecc7bd0f75381b53aa41d0eca2c
Michael Voznesensky <voznesenskym@gmail.com>
2022-06-20 05:55:57 +0000

    Fix tests

--- tests/test_repros.py
+++ tests/test_repros.py
@@ -892,7 +892,7 @@ class ReproTests(torchdynamo.testing.TestCase):
                 self.assertTrue(same(model(a, b, c, d), correct))
 
         self.assertEqual(cnt.frame_count, ifdyn(3, 2))
-        self.assertEqual(cnt.op_count, ifdyn(35, 28))
+        self.assertEqual(cnt.op_count, ifdyn(36, 29))
 
     @patch.object(torchdynamo.config, "capture_scalar_outputs", False)
     def test_maml_no_item_capture(self):
@@ -908,7 +908,7 @@ class ReproTests(torchdynamo.testing.TestCase):
                 self.assertTrue(same(model(a, b, c, d), correct))
 
         self.assertEqual(cnt.frame_count, ifdyn(5, 4))
-        self.assertEqual(cnt.op_count, ifdyn(35, 28))
+        self.assertEqual(cnt.op_count, ifdyn(36, 29))
 
     def test_hf_model_output(self):
         ex = ModelOutput(a=torch.randn(10), b=torch.randn(10), c=torch.randn(10))
