7629909c13bdbace9a42f6e25ba4e5eb0715b297
YJ Shi <yuanjing@octoml.ai>
2022-06-21 13:54:07 -0700

    add meta schedule backend

--- torchdynamo/optimizations/backends.py
+++ torchdynamo/optimizations/backends.py
@@ -583,7 +583,10 @@ def tvm_compile(jit_mod, example_inputs, log_file=None, **kwargs):
 def tvm(subgraph):
     return subgraph.wrap_returns(
         tvm_compile_inner(
-            subgraph.scripted, subgraph.example_inputs, None, cuda=subgraph.is_cuda
+            subgraph.scripted,
+            subgraph.example_inputs,
+            tuning_option=None,
+            cuda=subgraph.is_cuda,
         )
     )
 
@@ -598,7 +601,21 @@ def ansor(subgraph):
         tvm_compile_inner(
             subgraph.scripted,
             subgraph.example_inputs,
-            subgraph.filename("ansor"),
+            tuning_option="auto_scheduler",
+            log_file=subgraph.filename("ansor"),
+            cuda=subgraph.is_cuda,
+        )
+    )
+
+
+@create_backend
+def tvm_meta_schedule(subgraph):
+    return subgraph.wrap_returns(
+        tvm_compile_inner(
+            subgraph.scripted,
+            subgraph.example_inputs,
+            tuning_option="meta_schedule",
+            trials=20000,
             cuda=subgraph.is_cuda,
         )
     )
@@ -611,10 +628,11 @@ def llvm_target():
     return "llvm -mcpu=core-avx2"
 
 
-def tvm_compile_inner(jit_mod, example_inputs, log_file, trials=20000, cuda=False):
+def tvm_compile_inner(
+    jit_mod, example_inputs, tuning_option=None, log_file=None, trials=20000, cuda=False
+):
     # based on functorch version in eager_compile.py
     import tvm
-    from tvm import auto_scheduler
     from tvm import relay
     from tvm.contrib import graph_executor
 
@@ -626,7 +644,12 @@ def tvm_compile_inner(jit_mod, example_inputs, log_file, trials=20000, cuda=Fals
     else:
         dev = tvm.cpu(0)
         target = tvm.target.Target(llvm_target())
-    if log_file is not None:
+
+    if tuning_option == "auto_scheduler":
+        from tvm import auto_scheduler
+
+        if log_file is None:
+            log_file = tempfile.NamedTemporaryFile()
         if not os.path.exists(log_file):
             tasks, task_weights = auto_scheduler.extract_tasks(
                 mod["main"], params, target
@@ -656,10 +679,43 @@ def tvm_compile_inner(jit_mod, example_inputs, log_file, trials=20000, cuda=Fals
                 opt_level=3, config={"relay.backend.use_auto_scheduler": True}
             ):
                 lib = relay.build(mod, target=target, params=params)
-    else:
+    elif tuning_option == "meta_schedule":
+        from tvm.meta_schedule import TuneConfig
+        from tvm.meta_schedule.database import JSONDatabase
+        from tvm.meta_schedule.tune import tune_relay
+        from os import path as osp
+
+        with tempfile.TemporaryDirectory() as work_dir:
+            if log_file != None:
+                assert osp.isdir(
+                    log_file
+                ), "TVM's meta_schedule requires a directory for storing log files."
+                work_dir = log_file
+            lib: tvm.runtime.Module = tune_relay(
+                mod=mod,
+                params=params,
+                target=target,
+                config=TuneConfig(
+                    strategy="evolutionary",
+                    num_trials_per_iter=32,
+                    max_trials_per_task=20000,
+                    max_trials_global=trials,
+                ),
+                work_dir=work_dir,
+                database=JSONDatabase(
+                    osp.join(work_dir, "workload.json"),
+                    osp.join(work_dir, "records.json"),
+                ),
+            )
+    elif tuning_option == None:
         # no autotuning (for debugging)
         with tvm.transform.PassContext(opt_level=10):
             lib = relay.build(mod, target=target, params=params)
+    else:
+        raise NotImplementedError(
+            "This tuning option is invalid/not implemented for torchdynamo's TVM-related backend. "
+            "There are three available options including None, auto_scheduler and meta_schedule."
+        )
 
     m = graph_executor.GraphModule(lib["default"](dev))
 
@@ -678,7 +734,7 @@ def tvm_compile_inner(jit_mod, example_inputs, log_file, trials=20000, cuda=Fals
             if arg.dim() != 0:
                 m.set_input(
                     f"inp_{idx}",
-                    tvm.nd.from_dlpack(torch.utils.dlpack.to_dlpack(arg)),
+                    tvm.nd.array(arg.numpy(), dev),
                 )
         m.run()
         return [to_torch_tensor(m.get_output(i)) for i in range(m.get_num_outputs())]
