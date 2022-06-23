49e4bbd4082c03417abd59c1db621f150c1e6ab1
Jason Ansel <jansel@jansel.net>
2022-06-22 19:49:05 -0700

    .

--- torchinductor/ir.py
+++ torchinductor/ir.py
@@ -1406,10 +1406,11 @@ class ComputedBuffer(Buffer):
             if len(tiled_ranges) > 1:
                 return (*tiled_ranges, reduce_ranges), body
 
-            # alternate tiling heuristic
-            tiled_ranges, call = self._tile_broadcasting(iter_ranges, body, strides)
-            if len(tiled_ranges) > 1:
-                return (*tiled_ranges, reduce_ranges), call
+            if config.triton.tile_broadcasting:
+                # alternate tiling heuristic
+                tiled_ranges, call = self._tile_broadcasting(iter_ranges, body, strides)
+                if len(tiled_ranges) > 1:
+                    return (*tiled_ranges, reduce_ranges), call
 
         return (iter_ranges, reduce_ranges), body
 
@@ -2410,8 +2411,12 @@ class StorageBox(MutableBox):
                 ),
                 data=self.data,
             ).get_read_writes()
+
             # TODO(jansel): this heuristic is a wild guess
-            if len(read_writes.reads) > 4 or len(self.inner_fn_str()) > 2000:
+            if (
+                len(read_writes.reads) > config.realize_reads_threshold
+                or len(self.inner_fn_str()) > config.realize_bytes_threshold
+            ):
                 self.realize()
 
 
