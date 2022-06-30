ec2e67b95bb014604b76f384c4e8badf58bae76f
Elias Ellison <elias.ellison@gmail.com>
2022-06-30 19:25:22 +0000

    update test

--- tests/test_torchinductor.py
+++ tests/test_torchinductor.py
@@ -458,7 +458,7 @@ class CommonTemplate:
             return torch.mean(a)
 
         self.common(
-            fn, ((torch.rand((10, 3, 352, 352), dtype=torch.float16, device="cuda"),))
+            fn, ((torch.rand((10, 3, 352, 352), dtype=torch.float16),))
         )
 
     def test_min_max_reduction(self):
