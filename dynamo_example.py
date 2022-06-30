d42337ad2432832f5e4a2576f16a315ca130ac4a
Jiewen Tan <jwtan@fb.com>
2022-06-30 00:26:59 +0000

    Add profiler to validate gradient hooks

--- dynamo_example.py
+++ dynamo_example.py
@@ -9,14 +9,16 @@ from typing import List
 import torchdynamo
 from torchdynamo.optimizations import BACKENDS
 
+from torch.profiler import profile, record_function, ProfilerActivity
+
 class ToyModel(nn.Module):
     def __init__(self):
         super(ToyModel, self).__init__()
-        self.net1 = nn.Linear(10, 10)
-        self.net2 = nn.Linear(10, 10)
-        self.net3 = nn.Linear(10, 10)
+        self.net1 = nn.Linear(10, 10000)
+        self.net2 = nn.Linear(10000, 10000)
+        self.net3 = nn.Linear(10000, 10000)
         self.relu = nn.ReLU()
-        self.net4 = nn.Linear(10, 5)
+        self.net4 = nn.Linear(10000, 5)
 
     def forward(self, x):
         return self.net4(self.relu(self.net3(self.relu(self.net2(self.relu(self.net1(x)))))))
@@ -113,25 +115,35 @@ def graph_break_compiler(gm: fx.GraphModule, example_inputs: List[torch.Tensor])
 
     return final_graph_module.forward  # return a python callable
 
+def hook(grad):
+  print("gradient hook fired")
+  grad + 1
+  return grad
+
 def demo_basic():
-    with torchdynamo.optimize(graph_break_compiler):
-      device = "cuda"
+    with profile(activities=[ProfilerActivity.CPU, ProfilerActivity.CUDA]) as prof:
+      with torchdynamo.optimize(graph_break_compiler):
+        device = "cuda"
 
-      # create model and move it to the device with id rank
-      model = ToyModel().to(device)
+        # create model and move it to the device with id rank
+        model = ToyModel().to(device)
+        for parameter in model.parameters():
+          parameter.register_hook(hook)
 
-      loss_fn = nn.MSELoss()
-      optimizer = optim.SGD(model.parameters(), lr=0.001)
+        loss_fn = nn.MSELoss()
+        optimizer = optim.SGD(model.parameters(), lr=0.001)
 
-      for i in range(5):
-          optimizer.zero_grad()
-          outputs = model(torch.randn(20, 10).to(device))
-          labels = torch.randn(20, 5).to(device)
-          loss = loss_fn(outputs, labels)
-          loss.backward()
-          optimizer.step()
+        for i in range(1):
+            optimizer.zero_grad()
+            outputs = model(torch.randn(20, 10).to(device))
+            labels = torch.randn(20, 5).to(device)
+            loss = loss_fn(outputs, labels)
+            loss.backward()
+            optimizer.step()
 
-          print(f"{os.getpid()}: iteration {i}, loss {loss}")
+            print(f"{os.getpid()}: iteration {i}, loss {loss}")
+
+    prof.export_chrome_trace("trace.json")
 
 if __name__ == "__main__":
     demo_basic()
