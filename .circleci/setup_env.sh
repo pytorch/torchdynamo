f44db48e03ce33f2eb48b9812c0a657b2842b507
Jason Ansel <jansel@jansel.net>
2022-07-10 15:04:10 -0700

    .

--- .circleci/setup_env.sh
+++ .circleci/setup_env.sh
@@ -1,35 +1,45 @@
 #!/usr/bin/env bash
+# based on https://github.com/pytorch/functorch/blob/main/.circleci/unittest/linux/scripts/setup_env.sh
 set -x
 set -e
 
 # This script is for setting up environment in which unit test is ran.
 # To speed up the CI time, the resulting environment is cached.
-#
-# Do not install PyTorch and functorch here, otherwise they also get cached.
 
-this_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 root_dir="$(git rev-parse --show-toplevel)"
 conda_dir="${root_dir}/conda"
 env_dir="${root_dir}/env"
+torchbench_dir="${root_dir}/torchbenchmark"
 
 cd "${root_dir}"
 
-case "$(uname -s)" in
-    Darwin*) os=MacOSX;;
-    *) os=Linux
-esac
-
 # 1. Install conda at ./conda
 if [ ! -d "${conda_dir}" ]; then
-    printf "* Installing conda\n"
+    printf "* Installing conda at ${conda_dir}\n"
+    case "$(uname -s)" in
+        Darwin*) os=MacOSX;;
+        *) os=Linux
+    esac
     wget -O miniconda.sh http://repo.continuum.io/miniconda/Miniconda3-latest-${os}-x86_64.sh
     bash ./miniconda.sh -b -f -p "${conda_dir}"
 fi
 eval "$(${conda_dir}/bin/conda shell.bash hook)"
 
-# 2. Create test environment at ./env
+rm -rf "${env_dir}" "${torchbench_dir}"
+
 if [ ! -d "${env_dir}" ]; then
-    printf "* Creating a test environment\n"
-    conda create --prefix "${env_dir}" -y python="$PARAMETERS_PYTHON_VERSION"
+    printf "* Creating a test environmen at ${env_dir}\n"
+    conda create --prefix "${env_dir}" -y python=3.8
+    conda activate "${env_dir}"
+    conda install -y git-lfs
+    conda install -y -c pytorch magma-cuda113
+    conda install -y pytorch torchvision torchaudio torchtext cudatoolkit=11.3 -c pytorch
+else
+    conda activate "${env_dir}"
 fi
-conda activate "${env_dir}"
+
+if [ ! -d "${torchbench_dir}" ]; then
+    printf "* Installing torchbench at ${torchbench_dir}\n"
+    git clone https://github.com/jansel/benchmark "${torchbench_dir}"
+    (cd "${torchbench_dir}" && python install.py)
+fi
