96fe423b91ef3ea80174a5a78e795347eee35800
Yunjie Pan <panyj@fb.com>
2022-07-18 18:00:57 +0000

    minimal_repro

--- torchinductor/triton_ops/tests/test_mm_fusion.py
+++ torchinductor/triton_ops/tests/test_mm_fusion.py
@@ -1,4 +1,4 @@
-from benchmarks_.microbenchmarks import model as model
+from benchmarks.microbenchmarks import model as model
 import torch
 import torchdynamo
 import torchinductor.config
@@ -7,11 +7,12 @@ from torchdynamo.testing import rand_strided
 
 torchinductor.config.debug = True
 torchinductor.config.triton.dense_indexing = True
+# torchinductor.config.triton.cudagraphs = False
 torch.manual_seed(0)
 
 class Func(object):
     @torchdynamo.optimize("inductor")
-    def mm(x, w, bias):
+    def mm(x, w):
         y =  torch.mm(x, w)
         return y
 
@@ -23,7 +24,7 @@ class Func(object):
 
     # relu(mm)
     @torchdynamo.optimize("inductor")
-    def mm_relu(x, w, bias):
+    def mm_relu(x, w):
         y =  torch.mm(x, w)
         return torch.relu(y)
 
@@ -35,50 +36,32 @@ class Func(object):
         return torch.relu(y)
 
 
-def test(shape, fusion_type="add"):
+def test():
     dtype=torch.float32
-    torch.manual_seed(0)
-    # allocate inputs, nchw
-    a = rand_strided((128, 9216), (9216, 1), device='cuda', dtype=dtype)
-    b = rand_strided((9216, 4096), (4096, 1), device='cuda', dtype=dtype)
-    bias = torch.randn((shape[0][0],shape[1][1]), dtype=dtype, device='cuda')
+    a = torch.ones((128, 9216), device='cuda', dtype=dtype) / 10
+    b = torch.ones((9216, 4096), device='cuda', dtype=dtype) / 10
 
-    if fusion_type == "":
-        mm_fusion = getattr(Func, f"mm")
-    else:
-        mm_fusion = getattr(Func, f"mm_{fusion_type}")
+    
+    mm_fusion = getattr(Func, f"mm_relu")
     # torchinductor from template
     torchinductor.config.triton.use_mm = True
     torchinductor.metrics.reset()
-    y = mm_fusion(a, b, bias)
+    y = mm_fusion(a, b)
     assert torchinductor.metrics.generated_kernel_count == 1, f"codegen #kernel != 1"
     # baseline
     # reset to force code gen new python code
     torchdynamo.reset()
     torchinductor.config.triton.use_mm = False
-    y_correct = mm_fusion(a, b, bias)
-    # print("y", y[0,0:128])
-    # print("y_correct", y_correct[0,0:128])
+    y_correct = mm_fusion(a, b)
+    print("y", y[0,0:128])
+    print("y_correct", y_correct[0,0:128])
     assert(same(y, y_correct, cos_similarity=True))
 
-fusion_types = ["", "add", "relu", "add_relu"]
 shapes = [
     # alexnet
     ([128, 9216], [9216, 4096]),
-    ([128, 4096], [4096, 4096]),
-    ([128, 4096], [4096, 1000]),
-    # BERT
-    ([2048, 768], [768, 768]),
-    ([2048, 768], [768, 3072]),
-    ([2048, 3072], [3072, 768]),
-    # hf_GPT2
-    ([1024, 768], [768, 768]),
-    ([1024, 768], [768, 3072]),
-    ([1024, 3072], [3072, 768]),
-    ([1024, 768], [768, 2304]),
 ]
-for fusion_type in fusion_types:
-    print(f"testing correctness of mm+{fusion_type}")
-    for id, shape in enumerate(shapes[0:1]):
-        test(shape, fusion_type)
-    print("passed")
+
+print(f"testing correctness of mm+relu")
+test()
+print("passed")
