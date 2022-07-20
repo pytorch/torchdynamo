3b587abb76f1e7e4d03ed9e703091c9b6312c66b
Animesh Jain <anijain@umich.edu>
2022-07-20 16:17:40 +0000

    CircleCI - Checking if tf32=False also fails

--- benchmarks/common.py
+++ benchmarks/common.py
@@ -685,48 +685,48 @@ class BenchmarkRunner:
             is_training, current_device, name
         )
         # TF32 can amplify minor noises. So, disable TF32 for accuracy comparison
-        with no_tf32():
-            with self.pick_grad(name, is_training):
-                mode = "train" if is_training else "eval"
-                sys.stdout.write(f"{current_device:4} {mode:5} {current_name:34} ")
-                sys.stdout.flush()
-                for submod in itertools.chain([model], model.modules()):
-                    assert not torchdynamo.utils.is_jit_model(submod)
-                torch.manual_seed(1337)
-                correct_result = model_iter_fn(
-                    copy.deepcopy(model), torchdynamo.utils.clone_inputs(example_inputs)
+        # with no_tf32():
+        with self.pick_grad(name, is_training):
+            mode = "train" if is_training else "eval"
+            sys.stdout.write(f"{current_device:4} {mode:5} {current_name:34} ")
+            sys.stdout.flush()
+            for submod in itertools.chain([model], model.modules()):
+                assert not torchdynamo.utils.is_jit_model(submod)
+            torch.manual_seed(1337)
+            correct_result = model_iter_fn(
+                copy.deepcopy(model), torchdynamo.utils.clone_inputs(example_inputs)
+            )
+
+            torch.manual_seed(1337)
+            if current_name not in self.non_deterministic_models:
+                correct_rerun_result = model_iter_fn(
+                    copy.deepcopy(model),
+                    torchdynamo.utils.clone_inputs(example_inputs),
                 )
-
-                torch.manual_seed(1337)
-                if current_name not in self.non_deterministic_models:
-                    correct_rerun_result = model_iter_fn(
-                        copy.deepcopy(model),
-                        torchdynamo.utils.clone_inputs(example_inputs),
-                    )
-                    if not same(correct_result, correct_rerun_result):
-                        print("INCORRECT - Variation in Eager runs itself")
-                        if not skip_accuracy_check:
-                            return sys.exit(-1)
-
-                torch.manual_seed(1337)
-                torchdynamo.reset()
-
-                try:
-                    with optimize_ctx:
-                        new_result = model_iter_fn(model, example_inputs)
-                except Exception:
-                    logging.exception("unhandled error")
-                    print("ERROR")
-                    return sys.exit(-1)
-                if current_name in self.non_deterministic_models:
-                    # This model has non-deterministic output so we cant
-                    # check correctness.
-                    # TODO(jansel): submit upstream fix for this
-                    pass
-                elif not same(correct_result, new_result, cos_similarity, tolerance):
-                    print("INCORRECT")
+                if not same(correct_result, correct_rerun_result):
+                    print("INCORRECT - Variation in Eager runs itself")
                     if not skip_accuracy_check:
                         return sys.exit(-1)
+
+            torch.manual_seed(1337)
+            torchdynamo.reset()
+
+            try:
+                with optimize_ctx:
+                    new_result = model_iter_fn(model, example_inputs)
+            except Exception:
+                logging.exception("unhandled error")
+                print("ERROR")
+                return sys.exit(-1)
+            if current_name in self.non_deterministic_models:
+                # This model has non-deterministic output so we cant
+                # check correctness.
+                # TODO(jansel): submit upstream fix for this
+                pass
+            elif not same(correct_result, new_result, cos_similarity, tolerance):
+                print("INCORRECT")
+                if not skip_accuracy_check:
+                    return sys.exit(-1)
             ok, total = Stats.reset_counters()
             results = []
 
@@ -985,8 +985,8 @@ def parse_args():
 
 def main(runner, original_dir=None):
     # We are primarily interested in tf32 datatype
-    torch.backends.cuda.matmul.allow_tf32 = True
-    torch.backends.cudnn.allow_tf32 = True
+    torch.backends.cuda.matmul.allow_tf32 = False
+    torch.backends.cudnn.allow_tf32 = False
 
     args = parse_args()
 
