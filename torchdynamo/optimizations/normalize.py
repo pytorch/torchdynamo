6208d461fdc5e43dab98c30e7e3adec90cebf8cd
Sherlock Huang <bahuang@fb.com>
2022-08-18 00:42:48 +0000

    Fix prefuse scheduler dependency issue (#811)
    * add pre fused nodes to blocked_nodes in case there are dependencies inside prefused nodes
    
    [ghstack-poisoned]

--- torchdynamo/optimizations/normalize.py
+++ torchdynamo/optimizations/normalize.py
@@ -391,6 +391,8 @@ class Functionalization(Transformer):
 def swap_node(graph, old_node, new_node):
     old_node.replace_all_uses_with(new_node)
     graph.erase_node(old_node)
+    assert not new_node.meta, "New node must not have meta populated"
+    new_node.meta = old_node.meta
 
 
 def normalize(gm: torch.fx.GraphModule):
