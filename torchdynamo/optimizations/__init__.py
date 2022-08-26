7c248092c7b375b3a522625e121899efe0e9d4bf
Edward Z. Yang <ezyang@fb.com>
2022-08-26 10:31:21 -0700

    Delete offline autotuner
    Signed-off-by: Edward Z. Yang <ezyangfb.com>
    
    ghstack-source-id: aec4355ce2e6cdc77089abf72a1e611a5d146a85
    Pull Request resolved: https://github.com/pytorch/torchdynamo/pull/1047

--- torchdynamo/optimizations/__init__.py
+++ torchdynamo/optimizations/__init__.py
@@ -1,7 +1,6 @@
 from .backends import BACKENDS
-from .inference import offline_autotuner
 from .training import create_aot_backends
 
 create_aot_backends()
 
-__all__ = ["offline_autotuner", "BACKENDS"]
+__all__ = ["BACKENDS"]
