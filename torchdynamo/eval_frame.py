a2665c5fe11b91bb8d811303df52acd47bce5af2
Animesh Jain <anijain@umich.edu>
2022-09-01 21:58:42 -0700

    Add warning on using context manager (#1112)
    * Add warning on using context manager
    
    * Fix tests

--- torchdynamo/eval_frame.py
+++ torchdynamo/eval_frame.py
@@ -103,6 +103,15 @@ class _TorchDynamoContext:
         patch_fn()
 
     def __enter__(self):
+        logging.warn(
+            (
+                "Using TorchDynamo with a context manager will be deprecated soon."
+                "Please read https://github.com/pytorch/torchdynamo#usage-example "
+                "to use TorchDynamo using an annotation."
+            )
+        )
+        if config.raise_on_ctx_manager_usage:
+            raise RuntimeError("TorchDynamo is used with a context manager")
         self.on_enter()
         self.prior = set_eval_frame(self.callback)
         self.backend_ctx = self.extra_ctx_ctor()
@@ -374,13 +383,14 @@ def explain(f, *args, **kwargs):
         nonlocal out_guards
         out_guards.append(guards)
 
-    with patch(f"{__name__}.most_recent_backend", None), optimize(
-        dynamo_graph_accumulating_compiler,
-        nopython=False,
-        guard_export_fn=guard_export_print,
-    ):
+    with patch(f"{__name__}.most_recent_backend", None):
+        opt_f = optimize(
+            dynamo_graph_accumulating_compiler,
+            nopython=False,
+            guard_export_fn=guard_export_print,
+        )(f)
         # TODO(voz): We may have instances of `f` that mutate inputs, we should track sideffects and reject.
-        f(*args, **kwargs)
+        opt_f(*args, **kwargs)
 
     graph_count = len(graphs)
 
@@ -463,11 +473,12 @@ def export(f, *args, aten_graph=False, **kwargs):
     flat_args, in_spec = pytree.tree_flatten(args)
 
     remove_from_cache(f)
-    with patch(f"{__name__}.most_recent_backend", None), optimize_assert(
-        dynamo_normalization_capturing_compiler, guard_export_fn=guard_export_print
-    ):
+    with patch(f"{__name__}.most_recent_backend", None):
+        opt_f = optimize_assert(
+            dynamo_normalization_capturing_compiler, guard_export_fn=guard_export_print
+        )(f)
         # TODO(voz): We may have instances of `f` that mutate inputs, we should track sideffects and reject.
-        result_traced = f(*args, **kwargs)
+        result_traced = opt_f(*args, **kwargs)
     remove_from_cache(f)
 
     assert graph is not None, "whole graph export entails exactly one call"
