564879517b731a8e57b39fe4eb192206ec5182f2
Michael Voznesensky <voznesenskym@gmail.com>
2022-09-21 06:12:43 +0000

    Another test

--- test/test_misc.py
+++ test/test_misc.py
@@ -2406,26 +2406,51 @@ class MiscTests(torchdynamo.testing.TestCase):
                 b_float32 = torch.rand((8, 8), device="cuda")
                 c_float32 = torch.rand((8, 8), device="cuda")
                 d_float32 = torch.rand((8, 8), device="cuda")
- 
+
                 with torch.autocast(device_type="cuda", dtype=torch.bfloat16):
                     e_float16 = torch.mm(a_float32, b_float32)
                     f_float16 = torch.mm(d_float32, e_float16)
                 return f_float16
 
+        module = MyModule()
+        real = module(torch.tensor([0.5]))
+        real_device = real.device
+        real_dtype = real.dtype
+
+        graph, guards = torchdynamo.export(module, torch.tensor([[0.0, 0], [0, 0]]))
+        exported = graph(torch.tensor([0.5]))
+        self.assertEqual(exported.device, real_device)
+        self.assertEqual(exported.dtype, real_dtype)
+
+        self.assertEqual(exported.device.type, "cuda")
+        self.assertEqual(exported.device.index, 0)
+        self.assertEqual(exported.dtype, torch.bfloat16)
+
+    def test_autocast_float64(self):
+        class MyModule(torch.nn.Module):
+            def forward(self, x):
+                a_float32 = torch.rand((8, 8), device="cuda")
+                b_float32 = torch.rand((8, 8), device="cuda")
+                c_float32 = torch.rand((8, 8), device="cuda")
+                d_float32 = torch.rand((8, 8), device="cuda")
+
+                with torch.autocast(device_type="cuda", dtype=torch.float64):
+                    e_float64 = torch.mm(a_float32, b_float32)
+                    f_float64 = torch.mm(d_float32, e_float64)
+                return f_float64
 
         module = MyModule()
         real = module(torch.tensor([0.5]))
         real_device = real.device
         real_dtype = real.dtype
-        
+
         graph, guards = torchdynamo.export(module, torch.tensor([[0.0, 0], [0, 0]]))
         exported = graph(torch.tensor([0.5]))
         self.assertEqual(exported.device, real_device)
         self.assertEqual(exported.dtype, real_dtype)
-        
-        self.assertEqual(exported.device.type, 'cuda')
+
         self.assertEqual(exported.device.index, 0)
-        self.assertEqual(exported.dtype, torch.bfloat16)
+        self.assertEqual(exported.dtype, torch.float64)
 
 
 class TestTracer(JitTestCase):
@@ -2455,4 +2480,3 @@ class TestTracer(JitTestCase):
         fn()
         opt_fn = torchdynamo.optimize("eager")(fn)
         opt_fn()
-
