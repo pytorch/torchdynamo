ee37391a8b5ddae46d7b0f6a93235b777740d9c3
albanD <desmaison.alban@gmail.com>
2022-09-29 17:54:25 -0400

    sympy only works for python 3.8+

--- setup.py
+++ setup.py
@@ -30,6 +30,19 @@ if "develop" in sys.argv:
         "benchmarks.microbenchmarks.*",
     ]
 
+install_requires = [
+    "torch>=1.12.0",
+    "numpy",
+    "tabulate",
+    "pyyaml",
+    "dill",
+    "jinja2",
+    "networkx",
+]
+
+if sys.version_info >= (3, 8):
+    install_requires.append("sympy")
+
 
 setup(
     name="torchdynamo",
@@ -43,16 +56,7 @@ setup(
     license="BSD-3",
     keywords="pytorch machine learning compilers",
     python_requires=">=3.7, <3.11",
-    install_requires=[
-        "torch>=1.12.0",
-        "numpy",
-        "tabulate",
-        "pyyaml",
-        "sympy",
-        "dill",
-        "jinja2",
-        "networkx",
-    ],
+    install_requires=install_requires,
     packages=find_packages(include=package_include),
     package_data={
         "torchinductor/codegen": ["*.h", "*.j2"],
