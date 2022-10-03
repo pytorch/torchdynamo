fbcf652d581493442be10a296b9dabfb134024af
Sherlock Huang <bahuang@fb.com>
2022-10-03 17:48:37 +0000

    Import aten.sgn's decomp from _refs

--- torchinductor/decomposition.py
+++ torchinductor/decomposition.py
@@ -85,6 +85,7 @@ decompositions = get_decompositions(
         aten.sigmoid_backward,
         aten.silu_backward,
         aten.slice_backward,
+        aten.sgn,
         aten._softmax,
         aten._softmax_backward_data,
         aten.stack,
@@ -269,11 +270,6 @@ def conj_physical(self):
 @register_decomposition([aten.lift, aten.detach_])
 def lift(self):
     return self
-
-
-@register_decomposition([aten.sgn])
-def sgn(self):
-    return torch.sign(self)
 
 
 @register_decomposition([aten.fill.Scalar])
