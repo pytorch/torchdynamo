a1705c5f3b58964fe52805ae5c691407a52df35d
Yanbo Liang <ybliang8@gmail.com>
2022-10-07 18:40:32 -0700

    Wrap torch.profiler.* with AutogradProfilerContextWrapperVariable (#1542)
    * Wrap torch.profiler.* with AutogradProfilerContextWrapperVariable
    
    * Rename

--- torchdynamo/variables/user_defined.py
+++ torchdynamo/variables/user_defined.py
@@ -21,7 +21,7 @@ from ..utils import is_namedtuple_cls
 from ..utils import namedtuple_fields
 from .base import MutableLocal
 from .base import VariableTracker
-from .misc import AutogradProfilerContextWrapperVariable
+from .misc import ProfilerContextWrapperVariable
 
 
 class UserDefinedVariable(VariableTracker):
@@ -84,7 +84,7 @@ class UserDefinedClassVariable(UserDefinedVariable):
         options = VariableTracker.propagate(self, args, kwargs.values())
 
         if self.value is torch.autograd.profiler.profile:
-            return AutogradProfilerContextWrapperVariable()
+            return ProfilerContextWrapperVariable()
         elif is_namedtuple_cls(self.value):
             fields = namedtuple_fields(self.value)
             items = list(args)
