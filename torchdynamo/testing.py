7f7c4b0928d43c2f50517f82af564fefd16b0598
Jason Ansel <jansel@jansel.net>
2022-10-10 16:12:02 -0700

    .

--- torchdynamo/testing.py
+++ torchdynamo/testing.py
@@ -32,15 +32,13 @@ log = logging.getLogger(__name__)
 
 
 def run_tests(needs=()):
+    from torch.testing._internal.common_utils import IS_WINDOWS
     from torch.testing._internal.common_utils import TEST_WITH_CROSSREF
     from torch.testing._internal.common_utils import TEST_WITH_TORCHDYNAMO
     from torch.testing._internal.common_utils import run_tests
 
-    if TEST_WITH_TORCHDYNAMO:
-        return  # cant dynamo dynamo
-
-    if TEST_WITH_CROSSREF:
-        return  # needs __torch_function__
+    if TEST_WITH_TORCHDYNAMO or IS_WINDOWS or TEST_WITH_CROSSREF:
+        return  # skip testing
 
     if isinstance(needs, str):
         needs = (needs,)
