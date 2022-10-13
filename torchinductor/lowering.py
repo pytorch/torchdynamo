276bcea06a3f31f0fdd2e44fe425e87ee5d874f4
Fabio Rocha <frocha@quansight.com>
2022-10-13 09:02:34 +0000

    Remove lowering for index_select and add decomp for index_{add,add_} (#1292)
    The decompositons and refs for these functions were added in core in https://github.com/pytorch/pytorch/pull/85002.
    They generate the same code as the hand-written lowering.
    
    [ghstack-poisoned]

--- torchinductor/lowering.py
+++ torchinductor/lowering.py
@@ -1598,29 +1598,6 @@ def index_put_(self, indices, values, accumulate=False):
     return self
 
 
-@register_lowering(aten.index_select, type_promote=False)
-def index_select(x, dim, indices):
-    x_loader = x.make_loader()
-    index_loader = indices.make_loader()
-    dim = _validate_dim(x, dim, 0)
-
-    sizes = list(x.get_size())
-    (sizes[dim],) = indices.get_size()
-
-    def fn(idx):
-        assert len(idx) == len(sizes)
-        idx = list(idx)
-        idx[dim] = ops.indirect_indexing(index_loader([idx[dim]]))
-        return x_loader(idx)
-
-    return Pointwise.create(
-        device=x.get_device(),
-        dtype=x.get_dtype(),
-        inner_fn=fn,
-        ranges=sizes,
-    )
-
-
 @register_lowering(aten.scatter_, type_promote=False)
 def scatter_(self, dim: int, index, src, *, reduce: str = None):
     if reduce == "add":
