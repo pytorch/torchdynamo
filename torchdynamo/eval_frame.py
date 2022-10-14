89717b34ae5dba15ec4b93a664a44773fed6a1d7
mlazos <mlazos@fb.com>
2022-10-14 14:19:29 -0700

    Improvements to logging each step of dynamo + inductor (#1650)
    * Step logging fixes

--- torchdynamo/eval_frame.py
+++ torchdynamo/eval_frame.py
@@ -19,7 +19,6 @@ from torch.nn.parallel.distributed import DistributedDataParallel
 
 from . import config
 from . import convert_frame
-from . import logging as torchdynamo_logging
 from . import skipfiles
 from . import utils
 from .exc import ResetRequired
@@ -83,11 +82,6 @@ def innermost_fn(fn):
         unaltered_fn = unaltered_fn._torchdynamo_orig_callable
         assert callable(unaltered_fn)
     return unaltered_fn
-
-
-@functools.lru_cache(None)
-def _step_logger():
-    return torchdynamo_logging.get_step_logger(log)
 
 
 class _TorchDynamoContext:
@@ -161,9 +155,6 @@ class _TorchDynamoContext:
 
         @functools.wraps(fn)
         def _fn(*args, **kwargs):
-            if self.first_ctx:
-                _step_logger()(logging.INFO, "torchdynamo begin tracing")
-
             on_enter()
             prior = set_eval_frame(callback)
             backend_ctx = backend_ctx_ctor()
@@ -173,8 +164,6 @@ class _TorchDynamoContext:
             finally:
                 set_eval_frame(prior)
                 backend_ctx.__exit__(None, None, None)
-                if self.first_ctx:
-                    _step_logger()(logging.INFO, "torchdynamo done tracing")
 
         # hooks to properly handle inlining
         if isinstance(self, DisableContext):
