a8e9c8a43420734765a005d4a572cecc2653e754
Jason Ansel <jansel@jansel.net>
2022-10-14 07:47:13 -0700

    More fixes for move to core
    This fixes some minor issues in https://github.com/pytorch/pytorch/pull/86950

--- copy_to_core.sh
+++ copy_to_core.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 set -ex
+cd `dirname $0`
+(cd ../pytorch && make setup_lint)
 
-rsync -ra ~/torchdynamo/torchdynamo/ ~/pytorch/torch/_dynamo
-rsync -ra ~/torchdynamo/torchinductor/ ~/pytorch/torch/_inductor
-rsync -ra ~/torchdynamo/test/{dynamo,inductor} ~/pytorch/test/
-rsync -ra ~/torchdynamo/benchmarks/ ~/pytorch/benchmarks/dynamo
+rsync -ra ./torchdynamo/ ../pytorch/torch/_dynamo
+rsync -ra ./torchinductor/ ../pytorch/torch/_inductor
+rsync -ra ./test/{dynamo,inductor} ../pytorch/test/
+rsync -ra ./benchmarks/ ../pytorch/benchmarks/dynamo
 
-for DIR in ~/pytorch/test/{dynamo,inductor} ~/pytorch/benchmarks/dynamo
+for DIR in ../pytorch/test/{dynamo,inductor} ../pytorch/benchmarks/dynamo
 do
+  # Rename everything
   find $DIR -name '*.py' | xargs -n1 -- sed -i 's/torchdynamo/torch._dynamo/g'
   find $DIR -name '*.py' | xargs -n1 -- sed -i 's/torchinductor/torch._inductor/g'
+  # Fix variable names
   find $DIR -name '*.py' | xargs -n1 -- sed -i 's/_torch[.]_inductor/_torchinductor/g'
+  # Fix github urls
   find $DIR -name '*.py' | xargs -n1 -- sed -i 's@pytorch/torch[.]_dynamo@pytorch/torchdynamo@g'
 done
 
-(cd ~/pytorch && (lintrunner -a || lintrunner -a))
+# run lintrunner twice to workaround:
+# error: Two different linters proposed changes for the same file
+(cd ../pytorch && (lintrunner -a || lintrunner -a))
