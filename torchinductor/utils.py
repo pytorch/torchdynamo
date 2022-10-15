2bf259308f3ab72d3ff06b724c750851e61990d6
Jason Ansel <jansel@jansel.net>
2022-10-14 21:29:09 -0700

    fix

--- torchinductor/utils.py
+++ torchinductor/utils.py
@@ -37,7 +37,7 @@ def has_triton():
     try:
         import triton
 
-        return triton is not None and torch.cuda.get_device_capability() >= (8, 0)
+        return triton is not None
     except ImportError:
         return False
 
